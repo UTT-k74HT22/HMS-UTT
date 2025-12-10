1ef06112b0ee3ad25118b503fe10649f62c3c8a1
DungDev <hoangdinhdung0205@gmail.com>
2025-12-10 07:38:31 +0700

    refactor all structure

--- HospitalManagement.WinUI/Controllers/IAuthController.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalManagement.WinUI.Controllers;
-
-public interface IAuthController
-{
-    void ShowLogin();
-    void ShowRegister();
-    Task HandleLoginAsync(string username, string password);
-    Task HandleRegisterAsync(string username, string password, string role);
-}
---
+++ HospitalManagement/Program.cs
@@ -0,0 +1,16 @@
+namespace HospitalManagement;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        // ApplicationConfiguration.Initialize();
+        // Application.Run(new Form1());
+
+        Console.WriteLine("Hospital Management System Started");
+
+    }    
+}
