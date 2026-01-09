6850cf6ce454ad9c87342b5fd5783e92b65c2d67
Dohoan-2511 <dohoan25112005@gmail.com>
2026-01-09 19:37:27 +0700

    Module Product : hoàn thiện 60 % nên tạm thời chuyển qua làm bath

---
+++ HospitalManagement/service/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using HospitalManagement.dto.request.Product;
+using HospitalManagement.dto.response;
+using HospitalManagement.dto.response.Product;
+
+namespace HospitalManagement.service;
+
+public interface IProductService
+{
+    void Create(CreateProductRequest request);
+    
+    void Update(string code, UpdateProductRequest request);
+    
+    void Delete(string code);
+
+    List<ProductResponse> GetAll();
+    
+    ProductResponse GetByCode(string code);
+    
+    ProductDetailResponse GetDetailByCode(string code);
+
+    // List<CategoryResponse> GetAllCategories();
+    // List<ManufacturerResponse> GetAllManufacturers();
+    List<BatchResponse> GetBatchesByProduct(long productId);
+}
