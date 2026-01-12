4881189e683a0f3bbe315912488b126ea3ad38c8
Dohoan-2511 <dohoan25112005@gmail.com>
2026-01-12 13:08:39 +0700

    chỉnh lại giao diện Product

--- HospitalManagement/view/ProductManagementPanel.Designer.cs
+++ HospitalManagement/view/ProductManagementPanel.Designer.cs
@@ -21,41 +21,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lblKeyword = new System.Windows.Forms.Label();
-            btnAdd = new System.Windows.Forms.Button();
-            btnEdit = new System.Windows.Forms.Button();
-            btnDelete = new System.Windows.Forms.Button();
-            txtKeyword = new System.Windows.Forms.TextBox();
-            btnDetail = new System.Windows.Forms.Button();
-            btnSearch = new System.Windows.Forms.Button();
-            lblStatus = new System.Windows.Forms.Label();
-            cbStatus = new System.Windows.Forms.ComboBox();
-            btnRefresh = new System.Windows.Forms.Button();
-            lblTotal = new System.Windows.Forms.Label();
-            dgvProducts = new System.Windows.Forms.DataGridView();
-            btnDowloadExcel = new System.Windows.Forms.Button();
-            btnImportExcel = new System.Windows.Forms.Button();
+            lblKeyword = new Label();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            txtKeyword = new TextBox();
+            btnDetail = new Button();
+            btnSearch = new Button();
+            lblStatus = new Label();
+            cbStatus = new ComboBox();
+            btnRefresh = new Button();
+            lblTotal = new Label();
+            dgvProducts = new DataGridView();
+            btnDowloadExcel = new Button();
+            btnImportExcel = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvProducts).BeginInit();
             SuspendLayout();
             // 
             // lblKeyword
             // 
             lblKeyword.AutoSize = true;
-            lblKeyword.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            lblKeyword.Location = new System.Drawing.Point(147, 26);
+            lblKeyword.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblKeyword.Location = new Point(147, 26);
             lblKeyword.Name = "lblKeyword";
-            lblKeyword.Size = new System.Drawing.Size(55, 15);
+            lblKeyword.Size = new Size(55, 15);
             lblKeyword.TabIndex = 11;
             lblKeyword.Text = "Từ khóa:";
             // 
             // btnAdd
             // 
-            btnAdd.BackColor = System.Drawing.Color.ForestGreen;
-            btnAdd.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnAdd.ForeColor = System.Drawing.Color.Transparent;
-            btnAdd.Location = new System.Drawing.Point(136, 54);
+            btnAdd.BackColor = Color.ForestGreen;
+            btnAdd.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdd.ForeColor = Color.Transparent;
+            btnAdd.Location = new Point(136, 54);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new System.Drawing.Size(102, 23);
+            btnAdd.Size = new Size(102, 23);
             btnAdd.TabIndex = 10;
             btnAdd.Text = "Thêm";
             btnAdd.UseVisualStyleBackColor = false;
@@ -63,12 +63,12 @@
             // 
             // btnEdit
             // 
-            btnEdit.BackColor = System.Drawing.Color.DarkOrange;
-            btnEdit.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnEdit.ForeColor = System.Drawing.Color.Transparent;
-            btnEdit.Location = new System.Drawing.Point(244, 54);
+            btnEdit.BackColor = Color.DarkOrange;
+            btnEdit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEdit.ForeColor = Color.Transparent;
+            btnEdit.Location = new Point(244, 54);
             btnEdit.Name = "btnEdit";
-            btnEdit.Size = new System.Drawing.Size(92, 23);
+            btnEdit.Size = new Size(92, 23);
             btnEdit.TabIndex = 9;
             btnEdit.Text = "Sửa";
             btnEdit.UseVisualStyleBackColor = false;
@@ -76,12 +76,12 @@
             // 
             // btnDelete
             // 
-            btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)((byte)192)), ((int)((byte)64)), ((int)((byte)0)));
-            btnDelete.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnDelete.ForeColor = System.Drawing.Color.Snow;
-            btnDelete.Location = new System.Drawing.Point(342, 54);
+            btnDelete.BackColor = Color.FromArgb(192, 64, 0);
+            btnDelete.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDelete.ForeColor = Color.Snow;
+            btnDelete.Location = new Point(342, 54);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new System.Drawing.Size(89, 23);
+            btnDelete.Size = new Size(89, 23);
             btnDelete.TabIndex = 8;
             btnDelete.Text = "Xóa";
             btnDelete.UseVisualStyleBackColor = false;
@@ -89,19 +89,19 @@
             // 
             // txtKeyword
             // 
-            txtKeyword.Location = new System.Drawing.Point(206, 22);
+            txtKeyword.Location = new Point(206, 22);
             txtKeyword.Name = "txtKeyword";
-            txtKeyword.Size = new System.Drawing.Size(230, 23);
+            txtKeyword.Size = new Size(230, 23);
             txtKeyword.TabIndex = 7;
             // 
             // btnDetail
             // 
-            btnDetail.BackColor = System.Drawing.SystemColors.ControlDark;
-            btnDetail.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnDetail.ForeColor = System.Drawing.Color.Snow;
-            btnDetail.Location = new System.Drawing.Point(437, 54);
+            btnDetail.BackColor = SystemColors.ControlDark;
+            btnDetail.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDetail.ForeColor = Color.Snow;
+            btnDetail.Location = new Point(437, 54);
             btnDetail.Name = "btnDetail";
-            btnDetail.Size = new System.Drawing.Size(102, 23);
+            btnDetail.Size = new Size(102, 23);
             btnDetail.TabIndex = 6;
             btnDetail.Text = "Chi tiết";
             btnDetail.UseVisualStyleBackColor = false;
@@ -109,12 +109,12 @@
             // 
             // btnSearch
             // 
-            btnSearch.BackColor = System.Drawing.SystemColors.HotTrack;
-            btnSearch.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnSearch.ForeColor = System.Drawing.SystemColors.Control;
-            btnSearch.Location = new System.Drawing.Point(442, 21);
+            btnSearch.BackColor = SystemColors.HotTrack;
+            btnSearch.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearch.ForeColor = SystemColors.Control;
+            btnSearch.Location = new Point(442, 21);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new System.Drawing.Size(48, 23);
+            btnSearch.Size = new Size(48, 23);
             btnSearch.TabIndex = 5;
             btnSearch.Text = "Tìm";
             btnSearch.UseVisualStyleBackColor = false;
@@ -123,31 +123,31 @@
             // lblStatus
             // 
             lblStatus.AutoSize = true;
-            lblStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            lblStatus.Location = new System.Drawing.Point(578, 25);
+            lblStatus.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblStatus.Location = new Point(578, 25);
             lblStatus.Name = "lblStatus";
-            lblStatus.Size = new System.Drawing.Size(65, 15);
+            lblStatus.Size = new Size(65, 15);
             lblStatus.TabIndex = 4;
             lblStatus.Text = "Trạng thái:";
             // 
             // cbStatus
             // 
-            cbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            cbStatus.DropDownStyle = ComboBoxStyle.DropDownList;
             cbStatus.FormattingEnabled = true;
-            cbStatus.Location = new System.Drawing.Point(647, 22);
+            cbStatus.Location = new Point(647, 22);
             cbStatus.Name = "cbStatus";
-            cbStatus.Size = new System.Drawing.Size(121, 23);
+            cbStatus.Size = new Size(121, 23);
             cbStatus.TabIndex = 3;
             cbStatus.SelectedIndexChanged += cbStatus_SelectedIndexChanged;
             // 
             // btnRefresh
             // 
-            btnRefresh.BackColor = System.Drawing.SystemColors.ControlDark;
-            btnRefresh.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnRefresh.ForeColor = System.Drawing.SystemColors.Control;
-            btnRefresh.Location = new System.Drawing.Point(555, 54);
+            btnRefresh.BackColor = SystemColors.ControlDark;
+            btnRefresh.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRefresh.ForeColor = SystemColors.Control;
+            btnRefresh.Location = new Point(555, 54);
             btnRefresh.Name = "btnRefresh";
-            btnRefresh.Size = new System.Drawing.Size(102, 23);
+            btnRefresh.Size = new Size(102, 23);
             btnRefresh.TabIndex = 2;
             btnRefresh.Text = "Refresh";
             btnRefresh.UseVisualStyleBackColor = false;
@@ -156,31 +156,31 @@
             // lblTotal
             // 
             lblTotal.AutoSize = true;
-            lblTotal.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            lblTotal.Location = new System.Drawing.Point(61, 518);
+            lblTotal.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTotal.Location = new Point(61, 518);
             lblTotal.Name = "lblTotal";
-            lblTotal.Size = new System.Drawing.Size(108, 15);
+            lblTotal.Size = new Size(108, 15);
             lblTotal.TabIndex = 1;
             lblTotal.Text = "Tổng số sản phẩm:";
             // 
             // dgvProducts
             // 
             dgvProducts.AllowUserToAddRows = false;
-            dgvProducts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right));
-            dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvProducts.Location = new System.Drawing.Point(61, 103);
+            dgvProducts.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProducts.Location = new Point(61, 100);
             dgvProducts.Name = "dgvProducts";
-            dgvProducts.Size = new System.Drawing.Size(2177, 814);
+            dgvProducts.Size = new Size(1124, 399);
             dgvProducts.TabIndex = 0;
             // 
             // btnDowloadExcel
             // 
-            btnDowloadExcel.BackColor = System.Drawing.Color.DarkCyan;
-            btnDowloadExcel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnDowloadExcel.ForeColor = System.Drawing.SystemColors.Control;
-            btnDowloadExcel.Location = new System.Drawing.Point(687, 54);
+            btnDowloadExcel.BackColor = Color.DarkCyan;
+            btnDowloadExcel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDowloadExcel.ForeColor = SystemColors.Control;
+            btnDowloadExcel.Location = new Point(687, 54);
             btnDowloadExcel.Name = "btnDowloadExcel";
-            btnDowloadExcel.Size = new System.Drawing.Size(121, 23);
+            btnDowloadExcel.Size = new Size(121, 23);
             btnDowloadExcel.TabIndex = 12;
             btnDowloadExcel.Text = "DowloadTemplate";
             btnDowloadExcel.UseVisualStyleBackColor = false;
@@ -188,12 +188,12 @@
             // 
             // btnImportExcel
             // 
-            btnImportExcel.BackColor = System.Drawing.Color.FromArgb(((int)((byte)64)), ((int)((byte)64)), ((int)((byte)0)));
-            btnImportExcel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnImportExcel.ForeColor = System.Drawing.SystemColors.Control;
-            btnImportExcel.Location = new System.Drawing.Point(814, 54);
+            btnImportExcel.BackColor = Color.FromArgb(64, 64, 0);
+            btnImportExcel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnImportExcel.ForeColor = SystemColors.Control;
+            btnImportExcel.Location = new Point(814, 54);
             btnImportExcel.Name = "btnImportExcel";
-            btnImportExcel.Size = new System.Drawing.Size(102, 23);
+            btnImportExcel.Size = new Size(102, 23);
             btnImportExcel.TabIndex = 13;
             btnImportExcel.Text = "Import";
             btnImportExcel.UseVisualStyleBackColor = false;
@@ -201,8 +201,8 @@
             // 
             // ProductManagementPanel
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(btnImportExcel);
             Controls.Add(btnDowloadExcel);
             Controls.Add(dgvProducts);
@@ -217,7 +217,8 @@
             Controls.Add(btnEdit);
             Controls.Add(btnAdd);
             Controls.Add(lblKeyword);
-            Size = new System.Drawing.Size(1210, 570);
+            Name = "ProductManagementPanel";
+            Size = new Size(1210, 570);
             Load += ProductManagementPanel_Load;
             ((System.ComponentModel.ISupportInitialize)dgvProducts).EndInit();
             ResumeLayout(false);
