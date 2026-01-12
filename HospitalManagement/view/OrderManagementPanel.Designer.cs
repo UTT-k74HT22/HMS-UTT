7f0f1d44c1893b3f8c51457ba4c6d212aba0470c
Dohoan-2511 <dohoan25112005@gmail.com>
2026-01-12 10:20:06 +0700

    Final

--- HospitalManagement/view/OrderManagementPanel.Designer.cs
+++ HospitalManagement/view/OrderManagementPanel.Designer.cs
@@ -28,115 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lblTotal = new System.Windows.Forms.Label();
-            dgvOrders = new System.Windows.Forms.DataGridView();
-            btnCreate = new System.Windows.Forms.Button();
-            btnView = new System.Windows.Forms.Button();
-            btnConfirm = new System.Windows.Forms.Button();
-            btnCancel = new System.Windows.Forms.Button();
-            btnSearch = new System.Windows.Forms.Button();
-            btnRefresh = new System.Windows.Forms.Button();
-            txtSearch = new System.Windows.Forms.TextBox();
+            lblTotal = new Label();
+            dgvOrders = new DataGridView();
+            btnCreate = new Button();
+            btnView = new Button();
+            btnConfirm = new Button();
+            btnCancel = new Button();
+            btnSearch = new Button();
+            btnRefresh = new Button();
+            txtSearch = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dgvOrders).BeginInit();
             SuspendLayout();
             // 
             // lblTotal
             // 
             lblTotal.AutoSize = true;
-            lblTotal.Location = new System.Drawing.Point(39, 574);
+            lblTotal.Location = new Point(39, 574);
             lblTotal.Name = "lblTotal";
-            lblTotal.Size = new System.Drawing.Size(110, 15);
+            lblTotal.Size = new Size(110, 15);
             lblTotal.TabIndex = 0;
             lblTotal.Text = "Tổng số đơn hàng: ";
             // 
             // dgvOrders
             // 
-            dgvOrders.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right));
-            dgvOrders.BackgroundColor = System.Drawing.SystemColors.Control;
-            dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvOrders.Location = new System.Drawing.Point(3, 85);
+            dgvOrders.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            dgvOrders.BackgroundColor = SystemColors.Control;
+            dgvOrders.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvOrders.Location = new Point(39, 73);
             dgvOrders.Name = "dgvOrders";
-            dgvOrders.Size = new System.Drawing.Size(3737, 472);
+            dgvOrders.Size = new Size(1255, 472);
             dgvOrders.TabIndex = 1;
             // 
             // btnCreate
             // 
-            btnCreate.BackColor = System.Drawing.Color.Silver;
-            btnCreate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnCreate.Location = new System.Drawing.Point(246, 33);
+            btnCreate.BackColor = Color.Silver;
+            btnCreate.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCreate.Location = new Point(246, 33);
             btnCreate.Name = "btnCreate";
-            btnCreate.Size = new System.Drawing.Size(75, 34);
+            btnCreate.Size = new Size(75, 34);
             btnCreate.TabIndex = 2;
             btnCreate.Text = "Tạo đơn ";
             btnCreate.UseVisualStyleBackColor = false;
             // 
             // btnView
             // 
-            btnView.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            btnView.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnView.Location = new System.Drawing.Point(336, 32);
+            btnView.BackColor = SystemColors.ActiveBorder;
+            btnView.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnView.Location = new Point(336, 32);
             btnView.Name = "btnView";
-            btnView.Size = new System.Drawing.Size(86, 35);
+            btnView.Size = new Size(86, 35);
             btnView.TabIndex = 3;
             btnView.Text = "Xem chi tiết ";
             btnView.UseVisualStyleBackColor = false;
             // 
             // btnConfirm
             // 
-            btnConfirm.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            btnConfirm.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnConfirm.Location = new System.Drawing.Point(445, 33);
+            btnConfirm.BackColor = SystemColors.ActiveBorder;
+            btnConfirm.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnConfirm.Location = new Point(445, 32);
             btnConfirm.Name = "btnConfirm";
-            btnConfirm.Size = new System.Drawing.Size(75, 34);
+            btnConfirm.Size = new Size(75, 34);
             btnConfirm.TabIndex = 4;
             btnConfirm.Text = "Xác nhận";
             btnConfirm.UseVisualStyleBackColor = false;
             // 
             // btnCancel
             // 
-            btnCancel.BackColor = System.Drawing.Color.IndianRed;
-            btnCancel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnCancel.Location = new System.Drawing.Point(539, 33);
+            btnCancel.BackColor = Color.IndianRed;
+            btnCancel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCancel.Location = new Point(539, 32);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new System.Drawing.Size(75, 34);
+            btnCancel.Size = new Size(75, 34);
             btnCancel.TabIndex = 5;
             btnCancel.Text = "Hủy đơn";
             btnCancel.UseVisualStyleBackColor = false;
             // 
             // btnSearch
             // 
-            btnSearch.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            btnSearch.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnSearch.Location = new System.Drawing.Point(843, 33);
+            btnSearch.BackColor = SystemColors.ActiveCaption;
+            btnSearch.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearch.Location = new Point(843, 30);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new System.Drawing.Size(70, 34);
+            btnSearch.Size = new Size(70, 34);
             btnSearch.TabIndex = 6;
             btnSearch.Text = "Tìm kiếm";
             btnSearch.UseVisualStyleBackColor = false;
             // 
             // btnRefresh
             // 
-            btnRefresh.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            btnRefresh.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnRefresh.Location = new System.Drawing.Point(948, 29);
+            btnRefresh.BackColor = SystemColors.ActiveBorder;
+            btnRefresh.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRefresh.Location = new Point(928, 32);
             btnRefresh.Name = "btnRefresh";
-            btnRefresh.Size = new System.Drawing.Size(75, 31);
+            btnRefresh.Size = new Size(75, 31);
             btnRefresh.TabIndex = 7;
             btnRefresh.Text = "Làm mới";
             btnRefresh.UseVisualStyleBackColor = false;
             // 
             // txtSearch
             // 
-            txtSearch.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            txtSearch.Location = new System.Drawing.Point(635, 37);
+            txtSearch.BackColor = SystemColors.InactiveCaption;
+            txtSearch.Location = new Point(635, 37);
             txtSearch.Name = "txtSearch";
-            txtSearch.Size = new System.Drawing.Size(202, 23);
+            txtSearch.Size = new Size(202, 23);
             txtSearch.TabIndex = 8;
             // 
             // OrderManagementPanel
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(txtSearch);
             Controls.Add(btnRefresh);
             Controls.Add(btnSearch);
@@ -146,7 +146,8 @@
             Controls.Add(btnCreate);
             Controls.Add(dgvOrders);
             Controls.Add(lblTotal);
-            Size = new System.Drawing.Size(1357, 613);
+            Name = "OrderManagementPanel";
+            Size = new Size(1357, 613);
             ((System.ComponentModel.ISupportInitialize)dgvOrders).EndInit();
             ResumeLayout(false);
             PerformLayout();
